a0cda0fa3189529d00f39925a0e0042aa97f0bbb
Diego Soares <as.diego@live.com>
2021-07-02 17:40:12 -0300

    ordena por tamanho

--- Desafios-DIO/OrdenacaoDePalavrasPorTamanho.js
+++ Desafios-DIO/OrdenacaoDePalavrasPorTamanho.js
@@ -1,3 +1,16 @@
 for (let casos = parseInt(gets()); casos > 0; casos-- ) {
+    // recebe a linha de palavras
+    let entrada = gets().split(" ");
+
+    // ordena as palavras
+    entrada.sort(function(a, b){
+        // ASC  -> a.length - b.length
+        // DESC -> b.length - a.length
+        return b.length - a.length;
+      });
     
+    // cria uma nova string com as palavras em ordem
+    let saida = entrada.join(" ");
+    
+    console.log(saida)
 }
