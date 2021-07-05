9bec9a949d5278485302ee2b08be6ffd6848e1ec
Diego Soares <as.diego@live.com>
2021-07-05 17:13:52 -0300

    complete encontre a maior substring

--- Desafios-DIO/EncontreAMaiorSubstring.js
+++ Desafios-DIO/EncontreAMaiorSubstring.js
@@ -1,13 +1,49 @@
-for (let casos = parseInt(gets()); casos > 0; casos-- ) {
+function LCSubStr(X, Y, m, n) {
+    // Create a table to store
+    // lengths of longest common
+    // suffixes of substrings.
+    // Note that LCSuff[i][j]
+    // contains length of longest
+    // common suffix of
+    // X[0..i-1] and Y[0..j-1].
+    // The first row and first
+    // column entries have no
+    // logical meaning, they are
+    // used only for simplicity of program
 
-    // recebe a linha de palavras
-    let entrada = gets().split(" ");
+    let LCStuff =
+        Array(m + 1).fill().map(() => Array(n + 1).fill(0));
 
-    // ordena as palavras
-    entrada.sort().sort((a, b) => b.length - a.length);
+    // To store length of the longest
+    // common substring
+    let result = 0;
 
-    // cria uma nova string com as palavras em ordem
-    let saida = entrada.join(" ");
+    // Following steps build
+    // LCSuff[m+1][n+1] in bottom up fashion
+    for (i = 0; i <= m; i++) {
+        for (j = 0; j <= n; j++) {
+            if (i == 0 || j == 0)
+                LCStuff[i][j] = 0;
+            else if (X[i - 1] == Y[j - 1]) {
+                LCStuff[i][j] = LCStuff[i - 1][j - 1] + 1;
+                result = Math.max(result, LCStuff[i][j]);
+            } else
+                LCStuff[i][j] = 0;
+        }
+    }
+    return result;
+}
 
-    console.log(saida)
+// Driver Code
+
+while (true) {
+    let X = gets();
+    let Y = gets();
+
+    if (X == false || Y == false) { break }
+
+    let m = X.length;
+    let n = Y.length;
+
+    console.log(LCSubStr(X, Y, m, n));
 }
