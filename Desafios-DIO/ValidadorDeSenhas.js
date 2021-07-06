59d6099794e2b9a344c0454f173f2e6a74247630
Diego Soares <as.diego@live.com>
2021-07-06 16:16:55 -0300

    declara expressões regulares

--- Desafios-DIO/ValidadorDeSenhas.js
+++ Desafios-DIO/ValidadorDeSenhas.js
@@ -1,14 +1,26 @@
 function validadorDeSenha(senha) {
-    // Escreva aqui a lógica para validar a senha
-    return false;
-  }
-  
-  let senha = "";
-  
-  do {
+    // A senha deve conter, no mínimo, uma letra maiúscula, uma letra minúscula e um número;
+    // A mesma não pode ter nenhum caractere de pontuação, acentuação ou espaço;
+    // Além disso, a senha pode ter de 6 a 32 caracteres.
+
+    let maiuscula = /[A-Z]/; // deve conter ao menos uma letra maiúscula
+    let minuscula = /[a-z]/;
+    let numero = /\d/;
+    let verificaTamanho; // deve conter de 6 a 32 caracteres
+    let senhaValida = false;
+    let verifica = [...senha];
+
+    // regex 
+
+    return senhaValida;
+}
+
+let senha = "";
+
+do {
     senha = gets();
     if (senha !== "") {
-      let valido = validadorDeSenha(senha);
-      console.log("Senha " + (valido ? "valida." : "invalida."));    
+        let valido = validadorDeSenha(senha);
+        console.log("Senha " + (valido ? "valida." : "invalida."));
     }
-  } while (senha !== "");
+} while (senha !== "");
