ec9bbfc2130d54d90468d1548895d2a478ecca6d
Diego Soares <as.diego@live.com>
2021-07-14 18:50:06 -0300

    add pedra papel tesoura

---
+++ Desafios-DIO/pedraPapelTesouraLagartoESpock.js
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////
+///////// -- CÓDIGO ALTERADO -- /////////////
+////////////////////////////////////////////
+
+let dicionarioDeRegras = {
+    tesoura: ['papel','lagarto'],
+    papel: ['pedra','spock'],
+    pedra: ['lagarto','tesoura'],
+    lagarto: ['spock','papel'],
+    spock: ['tesoura','pedra']
+};
+
+let  fernanda = 'Fernanda';
+let  marcia = 'Marcia';
+
+let resultado;
+
+let N = parseInt(gets());
+
+for(let i = 0; i < N; i++){
+
+    let jogo = gets().split(' ');
+    if (jogo[0] === jogo[1]) {
+        resultado = 'empate';
+    } else {
+        resultado = (dicionarioDeRegras[jogo.includes(  ] ) ]) ?  fernanda : marcia;
+    }
+print(resultado);
+}
+
+
+//////////////////////////////////////////////
+///////// -- CÓDIGO FORNECIDO -- ////////////
+////////////////////////////////////////////
+/*
+
+let dicionarioDeRegras = {
+    tesoura: ['papel','lagarto'],
+    papel: ['pedra','spock'],
+    pedra: ['lagarto','tesoura'],
+    lagarto: ['spock','papel'],
+    spock: ['tesoura','pedra']
+};
+
+//finalize a declaração das jogadoras
+let  = 'fernanda';
+let  = 'marcia';
+
+let resultado;
+
+let N = parseInt(gets());
+
+// continue a solução
+
+for(   ){
+
+let     = gets().split(' ');
+if(   ===  )
+resultado = 'empate';
+else
+resultado = (dicionarioDeRegras[     .includes(  ] ) ) ?   :    ;
+
+print(resultado);
+}
+
+*/
