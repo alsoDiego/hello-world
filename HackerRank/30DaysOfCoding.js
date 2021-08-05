947e62e8669ae96a9fda78ef728be3013ca6045b
Diego Soares <as.diego@live.com>
2021-08-05 17:42:59 -0300

    30 days of coding, solve day 1

--- HackerRank/30DaysOfCoding.js
+++ HackerRank/30DaysOfCoding.js
@@ -7,3 +7,25 @@ function processData(inputString) {
     // Write the second line of output that prints the contents of 'inputString' here.
     console.log(inputString);
 }
+
+//Day 1: Data Types
+
+    // Declare second integer, double, and String variables.
+    let secondInt;
+    let secondFloat;
+    let secondString;
+    // Read and save an integer, double, and String to your variables.
+    secondInt = parseInt(readLine());
+    secondFloat = parseFloat(readLine());
+    secondString = readLine();
+    
+    // Print the sum of both integer variables on a new line.
+    console.log(i + secondInt);
+    // Print the sum of the double variables on a new line.
+    let sum = d + secondFloat;
+    console.log(sum.toPrecision(2));
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s + secondString);
+
+    
