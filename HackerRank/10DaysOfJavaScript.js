d2b33626727568ffa6fe85b416f2629d87325e7d
Diego Soares <as.diego@live.com>
2021-08-06 10:34:04 -0300

    10 days of js, solve day 1 challenge 3

--- HackerRank/10DaysOfJavaScript.js
+++ HackerRank/10DaysOfJavaScript.js
@@ -95,3 +95,13 @@ function factorial(n) {
     
     return result
 }
+
+// Day 1: Let and Const
+
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    let r = parseFloat(readLine());
+    // Print the area of the circle:
+    console.log(PI * r ** 2);
+    // Print the perimeter of the circle:
+    console.log(2 * PI * r);
