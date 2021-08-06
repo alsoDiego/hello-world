b5ea3b833822028182964f528aa7989c45650ab8
Diego Soares <as.diego@live.com>
2021-08-06 10:03:49 -0300

    10 days of js, solve day 1 challenge 1

--- HackerRank/10DaysOfJavaScript.js
+++ HackerRank/10DaysOfJavaScript.js
@@ -48,3 +48,35 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
     console.log(firstString + secondString);
 }
+
+// Day 1: Arithmetic Operators
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length * width;
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = 2 * (length + width);
+    return perimeter;
+}
