61337472fb472d537904e399e4eeae911c14b97e
Diego Soares <as.diego@live.com>
2021-08-09 20:45:49 -0300

    10 days of js, solve day 2 challenge 1

--- HackerRank/10DaysOfJavaScript.js
+++ HackerRank/10DaysOfJavaScript.js
@@ -105,3 +105,18 @@ function factorial(n) {
     console.log(PI * r ** 2);
     // Print the perimeter of the circle:
     console.log(2 * PI * r);
+
+// Day 2: If-Else
+function getGrade(score) {
+    let grade;
+    // Write your code here
+    
+    if (25 < score && score <= 30) { grade = "A"; }
+    if (20 < score && score <= 25) { grade = "B"; }
+    if (15 < score && score <= 20) { grade = "C"; }
+    if (10 < score && score <= 15) { grade = "D"; }
+    if (5  < score && score <= 10) { grade = "E"; }
+    if (0  <= score && score <= 5) { grade = "F"; }
+    
+    return grade;
+}
