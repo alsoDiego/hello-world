1fa804d75b6f0ecba91028d6771f22a12ca292df
Diego Soares <as.diego@live.com>
2021-08-09 21:02:11 -0300

    10 days of js, solve day 2 challenge 2

--- HackerRank/10DaysOfJavaScript.js
+++ HackerRank/10DaysOfJavaScript.js
@@ -120,3 +120,26 @@ function getGrade(score) {
     
     return grade;
 }
+
+// Day 2: Switch Case
+function getLetter(s) {
+    let letter;
+    // Write your code here
+        switch (true) {
+        case 'aeiou'.includes(s[0]):
+            letter = 'A';
+            break;
+        case 'bcdfg'.includes(s[0]):
+            letter = 'B';
+            break;
+        case 'hjklm'.includes(s[0]):
+            letter = 'C';
+            break;
+        case 'npqrstvwxyz'.includes(s[0]):
+            letter = 'D';
+            break;
+        default:
+            letter = 'Invalid input';
+    }
+    return letter;
+}
